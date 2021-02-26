96a07e09e4a854efa2df4a785190fb55c85a0f53
Nathalia Garcia <nathalia.garcia@prodigious.com>
2021-02-26 17:36:39 -0300

    ends the countdown component

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import styles from "../styles/components/Countdown.module.css";
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const [time, setTime] = useState(0.05 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -12,16 +15,25 @@ export function Countdown() {
   const [secondLeft, secondRigth] = String(seconds).padStart(2, "0").split("");
 
   function startCountdown() {
-    setActive(true);
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.05 * 60);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -36,13 +48,56 @@ export function Countdown() {
           <span>{secondRigth}</span>
         </div>
       </div>
-      <button
-        type="button"
-        className={styles.countDownButton}
-        onClick={startCountdown}
-      >
-        Iniciar um ciclo
-      </button>
+
+      {hasFinished ? (
+        <button disabled type="button" className={styles.countDownButton}>
+          Ciclo encerrado
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.00008 0.666626C4.40008 0.666626 0.666748 4.39996 0.666748 8.99996C0.666748 13.6 4.40008 17.3333 9.00008 17.3333C13.6001 17.3333 17.3334 13.6 17.3334 8.99996C17.3334 4.39996 13.6001 0.666626 9.00008 0.666626ZM7.33342 13.1666L3.16675 8.99996L4.34175 7.82496L7.33342 10.8083L13.6584 4.48329L14.8334 5.66663L7.33342 13.1666Z"
+              fill="#4CD62B"
+            />
+          </svg>
+        </button>
+      ) : (
+        <>
+          {!isActive ? (
+            <button
+              type="button"
+              className={styles.countDownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27 14.41L25.59 13L20 18.59L14.41 13L13 14.41L18.59 20L13 25.59L14.41 27L20 21.41L25.59 27L27 25.59L21.41 20L27 14.41Z"
+                  fill="#666666"
+                />
+              </svg>
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
